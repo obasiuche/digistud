62d7098ce84b3bbe2ea54f5143fa3aa64803de0a
Mercy Obasi <mercyobasiowen@gmail.com>
2022-07-06 11:51:10 +0100

    modified program to print to txt file instead of console

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 for x in range(2, 13):
     for y in range(1, 13):
-        print(f'{x} x {y} = {x * y}')
+        print(f'{x} x {y} = {x * y}', file="timestable.txt")
     print('==============================')
